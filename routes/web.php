4df05a7aef4d9bbdbcc824db5c29fa0bb36a46d7
Senior Major General Babar <107929664+Babar53@users.noreply.github.com>
2024-12-06 17:21:10 +0500

    spatie roles and permission package added

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('layouts.theme');
-});
-
 Auth::routes();
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
